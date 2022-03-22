f4417a941a16c3b155f971851f66aab992eb7a10
unknown <varunp211202@gmail.com>
2022-03-22 13:10:41 +0530

    worked on datatypes

---
+++ datatypes.js
@@ -0,0 +1,10 @@
+let name='varun'
+console.log(typeof(name))
+let num=5n
+console.log(typeof(num))
+let b=true
+console.log(typeof(b))
+let s1=Symbol('hello')
+console.log(typeof(s1))
+let s2='hello'
+console.log(name+s2)
