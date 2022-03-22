794ddb7b3990e7760c5ce4e20175e2ad039f2099
unknown <varunp211202@gmail.com>
2022-03-22 13:14:18 +0530

    Worked on operators

---
+++ operators.js
@@ -0,0 +1,15 @@
+//arithmetic operators
+var a=3,b=20
+console.log(`addition:${a+b}\nsubtraction:${a-b}\nmultiplication:${a*b}\ndivision:${a/b}\nmodulo:${a%b}\nexponent:${b**a}`)
+let c=100,d=100,z
+z=--c
+console.log(`z(c):${z} c:${c}`)
+z=d--
+console.log(`z(d):${z} d:${d}`)
+//assignment operators
+c=9,d=2
+console.log(c>>>=d)
+c=-9
+console.log(c>>>=d)
+c=9
+console.log(`${c^=d} ${c**=d}`)
